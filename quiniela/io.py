b8886b302096ef49db9809388c43aaf70ff8641e
orb-spider <harrywh7@gmail.com>
2024-11-02 19:56:02 +0100

    edited load_matchday to bring it up to date

--- quiniela/io.py
+++ quiniela/io.py
@@ -1,21 +1,24 @@
 import sqlite3
-
 import pandas as pd
-
 import settings
-
+from quiniela.structure import LaLigaDataframe
 
 def load_matchday(season, division, matchday):
     with sqlite3.connect(settings.DATABASE_PATH) as conn:
-        data = pd.read_sql(f"""
-            SELECT * FROM Matches
-                WHERE season = '{season}'
-                  AND division = {division}
-                  AND matchday = {matchday}
-        """, conn)
+        data = pd.read_sql_query("SELECT * FROM Matches", conn)
     if data.empty:
         raise ValueError("There is no matchday data for the values given")
-    return data
+    master = LaLigaDataframe(data.copy())
+    master.generate_features()
+    final = master.generate_matchday_dataframe()
+    matchday = final.df.loc[
+        (final.df.season == season) & 
+        (final.df.division == division) &
+        (final.df.matchday == matchday)]
+    
+    print(matchday)
+    return final.df
+
 
 
 def load_historical_data(seasons):
