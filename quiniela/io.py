384d0c4e0cdce2035c8c488f300bb4c561069cba
orb-spider <harrywh7@gmail.com>
2024-11-03 16:45:22 +0100

    formatting

--- quiniela/io.py
+++ quiniela/io.py
@@ -6,12 +6,15 @@ from quiniela.structure import LaLigaDataframe
 
 def load_matchday(season, division, matchday):
 
-    print(f"Loading matchday {matchday} in season {season}, division {division}...")
+    print(f"Loading matchday {matchday} +\
+          in season {season}, division {division}...")
 
     actual_season = int(season[-4:])
     prev_10_season = int(season[-4:])-10
-    seasons_tuple = tuple(f"{year}-{year + 1}" for year in range(prev_10_season, actual_season))    
-
+    seasons_tuple = (
+        tuple(f"{year}-{year + 1}" for year in range(prev_10_season,
+                                                     actual_season))
+    )
     with sqlite3.connect(settings.DATABASE_PATH) as conn:
         data = pd.read_sql(f"""
                 SELECT * FROM Matches
@@ -50,4 +53,7 @@ def load_historical_data(seasons):
 def save_predictions(predictions):
 
     with sqlite3.connect(settings.DATABASE_PATH) as conn:
-        predictions.to_sql(name="Predictions", con=conn, if_exists="append", index=False)
+        predictions.to_sql(name="Predictions",
+                           con=conn,
+                           if_exists="append",
+                           index=False)
